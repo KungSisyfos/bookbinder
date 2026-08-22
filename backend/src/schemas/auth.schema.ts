b0135df61640318f4600898b36266b0f3093aad6
Philip Andersson <82831087+KungSisyfos@users.noreply.github.com>
2026-08-22 14:07:47 +0200

    Validation (#3)
    * register login simple validation
    
    * impletement zod validation on register and login

---
+++ backend/src/schemas/auth.schema.ts
@@ -0,0 +1,12 @@
+import * as z from 'zod';
+
+export const registerSchema = z.object({
+	username: z.string().min(3).max(50),
+	email: z.email(),
+	password: z.string().min(6)
+});
+
+export const loginSchema = z.object({
+	usernameOrEmail: z.string().min(1),
+	password: z.string().min(6)
+});
